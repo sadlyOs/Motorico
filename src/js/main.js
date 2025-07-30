9a48dff14a35ab78994a0770e596ccedbea32fd0
sadlyOs <jukovkill@gmail.com>
2025-07-31 04:32:40 +0500

    header v2

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
+let toggle = false
 
+document.getElementById("burger").addEventListener("click", (e) => {
+    console.log('Нажата');
+    toggle = !toggle
+    document.getElementById("phoneMenu").style.display = toggle ? "flex" : "none"
+    // document.getElementById("burger").style.backgroundColor = "white"
+    
+})
