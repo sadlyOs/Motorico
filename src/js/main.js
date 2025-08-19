ba788bdc5e9f5d70258b73369a0ab3e73ec1d169
sadlyOs <jukovkill@gmail.com>
2025-08-19 09:36:42 +0500

    add mobile select

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,9 @@ const burger = document.getElementById("burger")
 burger.addEventListener("click", (e) => {
     const phoneMenu = document.getElementById("phoneMenu")
     toggle = !toggle
+    document.body.style.overflow = toggle ? "hidden" : ""
     burger.classList.toggle('white')
     phoneMenu.classList.toggle('hide')
+
+
 })
